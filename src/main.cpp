f9c2e45d1cb9e3d5272f986c77fb05af74028e7a
Danni Silk <danni!@silkypants.dev>
2026-02-15 15:18:35 +1030

    Break out BLE data into own files to close off feature
    There is more to be done in a larger scale, but ideally this should all be together

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
+#include "common.h"
+#include "ble/ble.h"
+
 #define OLED_GND 20
 #define OLED_VCC 10
 #define OLED_SCL 9
@@ -9,30 +12,6 @@
 
 // OPTION A: Standard SSD1306 (Try this first)
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/U8X8_PIN_NONE, /*clock =*/OLED_SCL, /*data =*/OLED_SDA);
-
-/// BLE
-
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
-
-// Unique IDs
-#define DEVICE_NAME "Pronoun Badge"
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define WRITE_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-#define READ_CHARACTERISTIC_UUID "a1b2c3d4-e5f6-47a8-b9c0-d1e2f3a4b5c6"
-
-// State
-bool isBlinking = false;
-unsigned long previousBlinkMillis = 0;
-const long blinkInterval = 500;
-
-BLECharacteristic *pStatusCharacteristic = nullptr;
-bool deviceConnected = false;
-
-void initBLE();
-///
 
 #include "danni.h"
 #include "she_her_data.h"
@@ -71,8 +50,6 @@ void setup()
   initBLE();
 }
 
-bool invert = false;
-
 void loop(void)
 {
   u8g2.clearBuffer();
@@ -92,106 +69,4 @@ void loop(void)
     previousBadgeMillis = currentMillis;
     currentBadge = (currentBadge + 1) % NUM_BADGES;
   }
-}
-
-
-// Server Callback to handle connection events
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-    Serial.println("Device Connected");
-  };
-
-  void onDisconnect(BLEServer *pServer)
-  {
-    deviceConnected = false;
-    Serial.println("Device Disconnected");
-    // Restart advertising so we can reconnect
-    BLEDevice::startAdvertising();
-  }
-};
-
-// Characteristic Callback to handle data sent from phone
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks
-{
-  void onWrite(BLECharacteristic *pCharacteristic)
-  {
-    // Get the raw data pointer and length
-    uint8_t *data = pCharacteristic->getData();
-    size_t len = pCharacteristic->getLength();
-
-    if (len > 0)
-    {
-      uint8_t command = data[0]; // Grab the first byte
-
-      // 0x01 (Hex/Byte) instead of '1' (Text)
-      if (command == 0x01)
-      {
-        isBlinking = true;
-        pStatusCharacteristic->setValue("Blinking");
-        pStatusCharacteristic->notify();
-      }
-      else if (command == 0x00)
-      {
-        isBlinking = false;
-        invert = false;
-        pStatusCharacteristic->setValue("Stopped");
-        pStatusCharacteristic->notify();
-      }
-    }
-  }
-};
-
-MyServerCallbacks myServerCallbacks;
-MyCharacteristicCallbacks myCharCallbacks;
-
-BLEDescriptor controlLabelDescriptor((uint16_t)0x2901);
-BLEDescriptor statusLabelDescriptor((uint16_t)0x2901);
-BLE2902 notifyDescriptor;
-
-void initBLE()
-{
-
-  // Initialize BLE
-  BLEDevice::init(DEVICE_NAME);
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(&myServerCallbacks);
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Control Characteristic
-  BLECharacteristic *pControlChar = pService->createCharacteristic(
-      WRITE_CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_WRITE);
-  pControlChar->setCallbacks(&myCharCallbacks);
-
-  controlLabelDescriptor.setValue("LED Blink Control (1=On, 0=Off)");
-  pControlChar->addDescriptor(&controlLabelDescriptor);
-
-  // Status Characteristic
-  pStatusCharacteristic = pService->createCharacteristic(
-      READ_CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_NOTIFY);
-
-  statusLabelDescriptor.setValue("Current Blinking Status");
-  pStatusCharacteristic->addDescriptor(&statusLabelDescriptor);
-
-// Configure the global notify descriptor (2902)
-  pStatusCharacteristic->addDescriptor(&notifyDescriptor);
-  pStatusCharacteristic->setValue("Stopped");
-
-  pService->start();
-
-  // Advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
-  pAdvertising->setMaxPreferred(0x12);
-  BLEDevice::startAdvertising();
-
-  Serial.println("System online and advertising...");
 }
